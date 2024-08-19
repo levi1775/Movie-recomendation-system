03e1112e433cea48d6606b1761ed35052ae0d5be
levi1775 <123142652+levi1775@users.noreply.github.com>
2024-08-19 19:22:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,60 +1,55 @@
-import os
-import json
-from PIL import Image
-
-import numpy as np
-import tensorflow as tf
 import streamlit as st
+import pickle
+import requests
 
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = 'https://image.tmdb.org/t/p/w500/' + poster_path
+    return full_path
 
-working_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = f"{working_dir}/trained_model/plant_disease_prediction_model.h5"
-# Load the pre-trained model
-model = tf.keras.models.load_model(model_path)
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
 
-# loading the class names
-class_indices = json.load(open(f"{working_dir}/class_indices.json"))
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        #fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
+    return recommended_movie_names,recommended_movie_posters
 
-# Function to Load and Preprocess the Image using Pillow
-def load_and_preprocess_image(image_path, target_size=(224, 224)):
-    # Load the image
-    img = Image.open(image_path)
-    # Resize the image
-    img = img.resize(target_size)
-    # Convert the image to a numpy array
-    img_array = np.array(img)
-    # Add batch dimension
-    img_array = np.expand_dims(img_array, axis=0)
-    # Scale the image values to [0, 1]
-    img_array = img_array.astype('float32') / 255.
-    return img_array
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+"Type or select a movie from the dropdown",
+  movie_list
+)
 
-
-# Function to Predict the Class of an Image
-def predict_image_class(model, image_path, class_indices):
-    preprocessed_img = load_and_preprocess_image(image_path)
-    predictions = model.predict(preprocessed_img)
-    predicted_class_index = np.argmax(predictions, axis=1)[0]
-    predicted_class_name = class_indices[str(predicted_class_index)]
-    return predicted_class_name
-
-
-# Streamlit App
-st.title('🌿Plant Disease Classifier')
-
-uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
-
-if uploaded_image is not None:
-    image = Image.open(uploaded_image)
-    col1, col2 = st.columns(2)
-
+if st.button('Recommend'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        resized_img = image.resize((150, 150))
-        st.image(resized_img)
-
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
     with col2:
-        if st.button('Classify'):
-            # Preprocess the uploaded image and predict the class
-            prediction = predict_image_class(model, uploaded_image, class_indices)
-            st.success(f'Prediction: {str(prediction)}')
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+
